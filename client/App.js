5a2cc853e43a0414d10b1f588bc94ed8dc553610
Mayank17M <mamgain.mayank01@gmail.com>
2022-01-23 17:24:57 +0530

    Connect to wallet

--- client/App.js
+++ client/App.js
@@ -1,24 +1,26 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import AuthScreen from './screens/AuthScreen';
-import RecordingScreen from './screens/RecordingScreen';
-import InsightScreen from './screens/InsightScreen';
+import AuthScreen from "./screens/AuthScreen";
+import RecordingScreen from "./screens/RecordingScreen";
+import InsightScreen from "./screens/InsightScreen";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Auth' component={AuthScreen} />
-        <Stack.Screen name='Recording' component={RecordingScreen} />
-        <Stack.Screen name='Insight' component={InsightScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Auth" component={AuthScreen} />
+          <Stack.Screen name="Recording" component={RecordingScreen} />
+          <Stack.Screen name="Insight" component={InsightScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
@@ -29,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- client/screens/AuthScreen.js
+++ client/screens/AuthScreen.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React from "react";
+import { Button, Text, Alert } from "react-native";
 
 const AuthScreen = ({ navigation }) => {
   return (
-    <Button
-      title='Go to Recording Screen'
-      onPress={() => navigation.navigate('Recording', { name: 'Joe' })}
-    />
+    <>
+      <Button
+        title="Connect with Metamask"
+        className="enableMetamaskButton"
+        onPress={() => {
+          if (typeof window.ethereum !== "undefined") {
+            console.log("MetaMask is installed!");
+            const accounts = ethereum.request({
+              method: "eth_requestAccounts",
+            });
+            const account = accounts[0];
+            Alert.alert(accounts);
+          }
+          //Will Start the metamask extension
+        }}
+      />
+      <Text>Account: </Text>
+      <Button
+        title="Go to Recording Screen"
+        onPress={() => navigation.navigate("Recording", { name: "Joe" })}
+      />
+    </>
   );
 };
 
-export default AuthScreen;
+export default AuthScreen;  
