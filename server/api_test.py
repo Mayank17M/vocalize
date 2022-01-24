30f1d89cf7ae6ddaa9bef63d11c146583e574b51
Mayank17M <mamgain.mayank01@gmail.com>
2022-01-24 18:16:15 +0530

    Complete prototype API

--- server/api_test.py
+++ server/api_test.py
@@ -5,3 +5,58 @@ import ast
 
 app=Flask(__name__)
 api = Api(app)
+
+#/users
+users_path='''wherever user data is stored (For example, "./data/users.csv")'''
+#/audio
+
+class Users(Resource):
+    def get(self):
+        data = pd.read_csv(users_path)
+        data = data.to_dict()
+        return {'data':data}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('audioId', required=True, type=int)
+        #parser.add_argument('name',required=True, type=str)
+        args = parser.parse_args()
+        #audio, #name maybe then uncomment the above parser statement
+        data = pd.read_csv(users_path) 
+
+        if args['audioId'] in data['audioId']:
+            return {
+            'message': f"{args['audioId']} already exists"
+            },409 
+        else:
+            data = data.append({
+                'audioId':str(args['audioId']) , 
+                #'name':args['name']
+            }, ignore_index = True)
+            return {'data':data.to_dict()},200 
+
+    def delete(self):
+        parser=reqparse.RequestParser()
+        parser.add_argument('audioId',required=True, type=int)
+        ##parser.add_argument('name',required=True, type=str)
+        args = parser.parse_args()
+
+        if args['audioId'] in data['audioId']:
+            data= data[data['audioId']!=args['audioId']]
+            data.to_csv(users_path, index=False) 
+            return {'data':data.to_dict()},200 
+        else:
+            return {'message':f"{args['audioId']} does not exist!"
+            },404 
+
+
+class Audio(Resource):
+    pass
+
+
+
+api.add_resource(Users,'/users')
+api.add_resource(Audio,'/audio')
+
+if __name__ == "__main__":
+    app.run(debug=True)
