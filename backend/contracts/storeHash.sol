018ce0b26999e2dc0079dac56f44bfe0f112678c
Mayank17M <mamgain.mayank01@gmail.com>
2022-02-02 23:25:47 +0530

    Store ipfs hash

---
+++ backend/contracts/storeHash.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+contract SaveAddress {
+    string ipfsHash;
+
+    function saveHash(string memory x) public {
+        ipfsHash = x;
+    }
+    function getHash(string memory y) public view returns (string memory x) {
+        return ipfsHash;
+    }
+}  
