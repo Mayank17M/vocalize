0f0f8f30f8514f42a03273db206780657a9e2516
Mayank17M <mamgain.mayank01@gmail.com>
2022-02-02 23:11:52 +0530

    Update API.py

--- server/api.py
+++ server/api.py
@@ -4,19 +4,6 @@ from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
 
-ACCESS_KEY = "ist_Dhztjg5pZmQeJuFgID3dinHYM7PqX2v7"
-BUCKET_KEY = "RVCXRFDKET8E"
-BUCKET_NAME = "Emotion recognition"
-
-# create a Streamer instance
-streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
-
-# send some data
-streamer.log("myNumber", 25)
-
-# flush and close the stream
-streamer.flush()
-
 app=Flask(__name__)
 api = Api(app)
 
@@ -24,31 +11,30 @@ api = Api(app)
 users_path= '''wherever user data is stored'''
 #/audio
 
-
 class Users(Resource):
     def get(self):
-        data = pd.read_csv(users_path)
+        data = pd.read_csv(users_path) #replace csv with whatever end path is used according to users_path
         data = data.to_dict()
-        return {'data':data}, 200
+        return {'data':data}, 200 
 
     def post(self):
         parser=reqparse.RequestParser()
         parser.add_argument('audioId',required=True, type=int)
         ##parser.add_argument('name',required=True, type=str)
         args = parser.parse_args()
-        # audio
+        #audio,
         data = pd.read_csv(users_path)
 
         if args['audioId'] in data['audioId']:
             return {
             'message': f"{args['audioId']} already exists"
-            },409 
+            },409 #might use a different number
         else:
             data = data.append({
                 'audioId':str(args['audioId']) ,
                 #'name':args['name']
             }, ignore_index = True)
-            return {'data':data.to_dict()},200 
+            return {'data':data.to_dict()},200
 
     def delete(self):
         parser=reqparse.RequestParser()
@@ -58,20 +44,30 @@ class Users(Resource):
 
         if args['audioId'] in data['audioId']:
             data= data[data['audioId']!=args['audioId']]
-            data.to_csv(users_path, index=False) #different file path instead csv may be needed
-            return {'data':data.to_dict()},200 #number may be different
+            data.to_csv(users_path, index=False)
+            return {'data':data.to_dict()},200 
         else:
             return {'message':f"{args['audioId']} does not exist!"
-            },404 #different number may be needed
+            },404 
 
 
 class Audio(Resource):
     pass
-
-
 
 api.add_resource(Users,'/users')
 api.add_resource(Audio,'/audio')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+ACCESS_KEY = "ist_Dhztjg5pZmQeJuFgID3dinHYM7PqX2v7"
+BUCKET_KEY = "RVCXRFDKET8E"
+BUCKET_NAME = "Emotion recognition"
+
+# create a Streamer instance
+streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
+
+streamer.log("myNumber", 25)
+
+# flush and close the stream
+streamer.flush()
