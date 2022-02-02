e91beaa81d27f029eddef4e1da6e995b91314e2e
Mayank17M <mamgain.mayank01@gmail.com>
2022-02-02 23:32:52 +0530

    ipfs init and create routes

---
+++ backend/models/images.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const imageSchema = mongoose.Schema({
+    label: {
+        type: String,
+    },
+    ipfsHash: {
+        type: String,
+    },
+    ipfsAddress: {
+        type: String,
+    },
+    transactionHash: {
+        type: String,
+    },
+    blockHash: {
+        type: String,
+    },
+}, {
+        timestamps: true,
+    });
+
+imageSchema.index({ label: 1 });
+
+
+module.exports = mongoose.model('Image', imageSchema);
