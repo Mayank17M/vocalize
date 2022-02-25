1c7c4838590798c57359df2534746eb1bd4b68d3
Mayank17M <mamgain.mayank01@gmail.com>
2022-02-25 13:28:45 +0530

    Add comments

--- client/App.js
+++ client/App.js
@@ -1,7 +1,7 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native"; //managing app state
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; //transition between screens
 
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar"; //control app status bar
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
